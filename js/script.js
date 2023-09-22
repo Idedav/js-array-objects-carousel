69ea9945c886dcac7bee94bb02b7dd35ef2e9cdd
Idedav <xdavide.correa@gmail.com>
2023-09-22 16:01:42 +0200

    [ADD] bonus 1-2

--- js/script.js
+++ js/script.js
@@ -27,8 +27,9 @@ const slicesImages = document.querySelector('.slices')
 const carouselImages = document.querySelector('.caurosel-img');
 let counterImg = 0
 // BUTTONS
-const btnTop = document.querySelector('.btn-top')
-const btnBot = document.querySelector('.btn-bottom')
+const btnTop = document.querySelector('.btn-top');
+const btnBot = document.querySelector('.btn-bottom');
+const btnInterval = document.querySelector('#interval');
 // CICLE
 carouselWrapper.forEach(key => {
     const image = key.image;
@@ -46,8 +47,8 @@ carouselWrapper.forEach(key => {
     slicesImages.innerHTML += `<img src=${image} class='slice'>`
 })
 
-    const slicesWrapper = document.getElementsByClassName('slice')
-    slicesWrapper[0].classList.add('active')
+    const slicesWrapper = document.getElementsByClassName('slice');
+    slicesWrapper[0].classList.add('active');
     const itemsWrapper = document.getElementsByClassName('item');
     itemsWrapper[0].classList.remove('hide')
 
@@ -82,3 +83,46 @@ carouselWrapper.forEach(key => {
         slicesWrapper[counterImg].classList.add('active')
         itemsWrapper[counterImg].classList.remove('hide')
     }
+
+
+    // ---------SET INTERVAL-------------
+    let clockOk = true;
+    let clockReverse;
+    let clock = setInterval(function(){
+        next();
+        if(counterImg != itemsWrapper.length - 1){
+            counterImg++
+        }else{
+            counterImg = 0
+        }
+        prev();
+    },2000);
+
+
+    btnInterval.addEventListener('click', function(){
+        if(clockOk == true){
+            clearInterval(clock);
+            clockReverse = setInterval(function(){
+                next();
+                if(counterImg != 0){
+                    counterImg--
+                }else{
+                    counterImg = itemsWrapper.length - 1
+                }
+                prev();
+            },2000)
+            clockOk = false
+        }else if(clockOk == false){
+            clearInterval(clockReverse);
+            clock = setInterval(function(){
+                next();
+                if(counterImg != itemsWrapper.length - 1){
+                    counterImg++
+                }else{
+                    counterImg = 0
+                }
+                prev();
+            },2000);
+            clockOk = true;
+        }
+    })
